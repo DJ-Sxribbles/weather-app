fa3601bea07e7f8b51f5588f21bc635251bf598a
Darlynna John <darlynna.john@gmail.com>
2022-11-20 16:55:59 -0700

    updated app.js
    added quotes around apiKey

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-let apiKey = cb856e0d75ef9e01d9881d38fabef8fe;
+let apiKey = "cb856e0d75ef9e01d9881d38fabef8fe";
 let apiURL =
   "https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}";
 let cityName = "London";
