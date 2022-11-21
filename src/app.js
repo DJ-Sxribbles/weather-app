bd30fbe55ffa27a90650ba9ac5018906839482cc
Darlynna John <darlynna.john@gmail.com>
2022-11-20 17:47:16 -0700

    updated app.js
    added function displayTemperature

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
+function displayTemperature(reponse) {
+  console.log(reponse.data);
+}
 let apiKey = "cb856e0d75ef9e01d9881d38fabef8fe";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
 
-console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
