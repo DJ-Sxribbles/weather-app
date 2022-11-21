45be6281a280b65deb14382181720e5752c868be
Darlynna John <darlynna.john@gmail.com>
2022-11-20 18:48:15 -0700

    updated app.js
    Added cityElement variable from the response data

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.date.name;
+  console.log(response.data);
 }
 let apiKey = "cb856e0d75ef9e01d9881d38fabef8fe";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
