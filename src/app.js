047e395668874b6a722d36a3d9fa00dc5c90d929
Darlynna John <darlynna.john@gmail.com>
2022-11-20 17:05:33 -0700

    updated app.js
    used back ticks for interpolation for apiURL

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 let apiKey = "cb856e0d75ef9e01d9881d38fabef8fe";
-let apiURL =
-  "https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}";
+let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
 let cityName = "London";
 
 console.log(apiURL);
