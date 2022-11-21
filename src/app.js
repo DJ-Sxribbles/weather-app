437e7991817c1a9679ca6bc99252dda89671a1d7
Darlynna John <darlynna.john@gmail.com>
2022-11-20 18:51:31 -0700

    updated app.js
    Revised data in response.data.name

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.date.name;
+  cityElement.innerHTML = response.data.name;
   console.log(response.data);
 }
 let apiKey = "cb856e0d75ef9e01d9881d38fabef8fe";
