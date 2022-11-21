807c7c036c23ec33fc59ed2a90d87be871bbb5b0
Darlynna John <darlynna.john@gmail.com>
2022-11-20 18:35:41 -0700

    updated app.js
    added variable temperatureElment with response data for temp

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-function displayTemperature(reponse) {
-  console.log(reponse.data);
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
 }
 let apiKey = "cb856e0d75ef9e01d9881d38fabef8fe";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
