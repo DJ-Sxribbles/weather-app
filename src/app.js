40388bf085ecdf0188971c18872bb8fd20177134
Darlynna John <darlynna.john@gmail.com>
2022-11-20 22:15:03 -0700

    Updated form and app.js
    Updated index.html form by adding autocomplete=off. Edited apiUrl by adding interpolation ${city} in app.js.

--- src/app.js
+++ src/app.js
@@ -55,7 +55,8 @@ function displayTemperature(response) {
 
 function search(city) {
   let apiKey = "cb856e0d75ef9e01d9881d38fabef8fe";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
   axios.get(apiUrl).then(displayTemperature);
 }
 
