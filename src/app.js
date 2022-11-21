b834b2560d970b72c2af146aa9bb6a30f03a4ba1
Darlynna John <darlynna.john@gmail.com>
2022-11-20 18:54:20 -0700

    updated app.js
    Updated units to metric in the apiUrl link

--- src/app.js
+++ src/app.js
@@ -6,6 +6,6 @@ function displayTemperature(response) {
   console.log(response.data);
 }
 let apiKey = "cb856e0d75ef9e01d9881d38fabef8fe";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
