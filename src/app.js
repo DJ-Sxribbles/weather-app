a1161fd4c082040de72ece14da9338c129f093b9
Darlynna John <darlynna.john@gmail.com>
2022-12-03 20:26:18 -0700

    Updated app.js
    Added new apiKey to call a daily forecast.

--- src/app.js
+++ src/app.js
@@ -69,9 +69,17 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "cb856e0d75ef9e01d9881d38fabef8fe";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
+  ß;
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let descriptionElement = document.querySelector("#description");
@@ -94,7 +102,7 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
-  console.log(response.data);
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -142,4 +150,3 @@ let celsiusLink = document.querySelector("#celsius");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
-displayForecast();
