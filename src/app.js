cd989c9c9df51b4794120d5a82f985358bef7761
Darlynna John <darlynna.john@gmail.com>
2022-12-03 20:43:06 -0700

    Updated apiKeys

--- src/app.js
+++ src/app.js
@@ -52,7 +52,7 @@ function displayForecast(response) {
                   alt="weather icon"
                   id="icon"
                 />
-                <div class="weather-forecast-temperatures">
+                <div class="">
                   <span class="weather-forecast-temp-max">${Math.round(
                     forecastDay.temp.max
                   )}°</span>
@@ -71,7 +71,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "cb856e0d75ef9e01d9881d38fabef8fe";
+  let apiKey = "ebef9ca4a8de66ed586fac628fade056";
   let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -106,7 +106,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "cb856e0d75ef9e01d9881d38fabef8fe";
+  let apiKey = "ebef9ca4a8de66ed586fac628fade056";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
